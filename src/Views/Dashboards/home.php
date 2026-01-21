9c7a84b292c199ec759d24dfaa8af09e2d1a7fca
Lahcen <akdiml309@gmail.com>
2026-01-21 10:02:29 +0100

    add : fullcalendar library

--- src/Views/Dashboards/home.php
+++ src/Views/Dashboards/home.php
@@ -6,6 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Legal Services Platform - Home</title>
     <link rel="stylesheet" href="src/Views/Public/style.css">
+    <!-- <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.20/index.global.min.js'></script>
+    <script>
+
+      document.addEventListener('DOMContentLoaded', function() {
+        var calendarEl = document.getElementById('calendar');
+        var calendar = new FullCalendar.Calendar(calendarEl, {
+          initialView: 'dayGridMonth'
+        });
+        calendar.render();
+      });
+
+    </script> -->
 </head>
     <?php require_once "../src/Views/public/header.html" ?>
 <body>
@@ -35,7 +47,13 @@
                 <div class="button-icon">📋</div>
                 <div class="button-label">Ajouter Un Employee</div>
             </a>
+
+            <!-- <a href="Statistiques" class="button-card">
+                <div class="button-icon">📊</div>
+                <div class="button-label">Calendar</div>
+            </a> -->
         </div>
+         <!-- <div id='calendar'></div> -->
     </main>
 
     <footer>
