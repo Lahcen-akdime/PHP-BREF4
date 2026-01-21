46ea10c48e52a1d6c8d96d85991033e24dbb2494
achrafelberkaoui <achrafelberkoaui@gmail.com>
2026-01-21 20:12:09 +0100

    ajoute construct et getters class user

--- app/models/User.php
+++ app/models/User.php
@@ -2,9 +2,33 @@
 namespace models;
 
 class User{
-    protected int $id ;
-    protected string $name ;
-    protected string $email;
-    protected string $password;
-    protected array $role;
+    protected ?int $id ;
+    protected ?string $name ;
+    protected ?string $email;
+    protected ?string $password;
+    protected ?array $role;
+
+    public function __construct(?int $id = null, ?string $email = null, ?string $password = null, ?array $role = null)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->role = $role;
+        $this->password = $password;
+    }
+
+    public function getId(): ?int{
+        return $this->$id;
+    }
+    public function getName(): ?string{
+        return $this->$name;
+    }
+    public function getPassword(): ?string{
+        return $this->$passowrd;
+    }
+    public function getRole(): ?string{
+        return $this->$role;
+    }
+    public function getEmail(): ?stirng{
+        return $this->$email;
+    }
 }
