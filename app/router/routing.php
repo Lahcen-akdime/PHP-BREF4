441395661d3b20d014a815c2112faff84ac90465
ouslimaneabdelilah <abdelilah.ouslimane@gmail.com>
2026-01-21 07:15:55 +0100

    feat : ajouter la logic pour creation en demande

--- app/router/routing.php
+++ app/router/routing.php
@@ -1,26 +1,32 @@
 <?php
-namespace router ;
-class Routing{
-    private static array $controllers = ["avocats"=>"avocatsController",
-                                        "huissier"=>"huissierController",
-                                        "home"=>"homeController",
-                                        "dashboard"=>"dashboardController",
-                                        "Statistiques"=>"statistiquesController",
-                                        "Create"=>"CreateController",
-                                        "DeleteAvocat"=>"DeleteAvocatController",
-                                        "DeleteHuissier"=>"DeleteHuissierController",
-                                        "editHuissier"=>"editHuissierController",
-                                        "editAvocat"=>"editAvocatController",
-                                        "json"=>"jsonController",
-                                        "client" => "ClientController",
-                                        "pagination"=>"paginationController"];
-    public static function dispatch(){
-        $page = $_GET['page'] ?? "home" ;
-        if(array_key_exists($page,self::$controllers)){
-        require_once __DIR__."/../Controller/".self::$controllers[$page].".php";
-        }
-        else{
-        echo "404";
+
+namespace router;
+
+class Routing
+{
+    private static array $controllers = [
+        "avocats" => "avocatsController",
+        "huissier" => "huissierController",
+        "home" => "homeController",
+        "dashboard" => "dashboardController",
+        "Statistiques" => "statistiquesController",
+        "Create" => "CreateController",
+        "DeleteAvocat" => "DeleteAvocatController",
+        "DeleteHuissier" => "DeleteHuissierController",
+        "editHuissier" => "editHuissierController",
+        "editAvocat" => "editAvocatController",
+        "json" => "jsonController",
+        "client" => "ClientController",
+        "demande" => "DemandeController",
+        "pagination" => "paginationController"
+    ];
+    public static function dispatch()
+    {
+        $page = $_GET['page'] ?? "home";
+        if (array_key_exists($page, self::$controllers)) {
+            require_once __DIR__ . "/../Controller/" . self::$controllers[$page] . ".php";
+        } else {
+            echo "404";
         }
     }
 }
